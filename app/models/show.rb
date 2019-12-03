85b670933f8ed249dfff43ace5edd4377f19f4c7
Nicholas Ng <nicholasykng@gmail.com>
2019-12-03 04:48:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where("rating = ?", self.highest_rating).first
   end
-  
+
   def self.lowest_rating
     Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where("rating = ?", self.lowest_rating).first
   end
 end
